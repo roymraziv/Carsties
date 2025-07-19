2d12b7bf0349fe39e28e4f93129b93108a77463b
Roy <roywmraziv@icloud.com>
2025-07-19 14:35:05 -0500

    feat: starting SearchService

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,37 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await DB.InitAsync("SearchDB", MongoClientSettings
+    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDBConnection")));
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .Key(x => x.Year, KeyType.Ascending)
+    .CreateAsync();
+
+app.Run();
